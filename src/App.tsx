7850af9f51dbcd531a7311f397f0b6eab37510b1
서진 <dltjwls0529@gmail.com>
2023-11-02 14:11:00 +0900

    🔧 setting : 패키지 설치, 라우팅 및 recoil 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>home</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
