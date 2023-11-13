432310837a93bd4b3e134be4bdf78ccfd1e1ae1b
서진 <dltjwls0529@gmail.com>
2023-11-13 10:56:21 +0900

    🐛 네비게이션 바 로고 네비게이트 추가

--- src/components/_common/NavBar.tsx
+++ src/components/_common/NavBar.tsx
@@ -13,7 +13,11 @@ const NavBar = () => {
   return (
     <div className="bg-white flex flex-col items-center pt-[10px] border-b-[1px] border-lightGray">
       <div className="w-[calc(100%-100px)] flex justify-between items-center gap-[36px]">
-        <Logo width="150" className="flex-shrink-0" />
+        <Logo
+          width="150"
+          className="flex-shrink-0 cursor-pointer"
+          onClick={() => nav("/")}
+        />
         <div className="h-[50px] flex gap-[36px] items-center mt-[10px]">
           <div
             className="font-regular text-smTitle cursor-pointer whitespace-nowrap"
