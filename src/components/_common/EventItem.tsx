94e28fd3a655259103063252382ab968c8e5ac18
서진 <dltjwls0529@gmail.com>
2023-11-15 02:22:24 +0900

    🐛 EventItem 종료 시 UI, 신청자 조회 UI 추가

--- src/components/_common/EventItem.tsx
+++ src/components/_common/EventItem.tsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CategoryBlock from "./CategoryBlock";
 import { ReactComponent as HeartIcon } from "../../assets/heart.svg";
+import { ReactComponent as ArrowCircleIcon } from "../../assets/My/arrow_circle_right.svg";
 
+interface Props extends EventPreviewType {
+  isRegist?: boolean;
+}
 const EventItem = ({
   programId,
   title,
@@ -12,12 +16,26 @@ const EventItem = ({
   remainingDays,
   goal,
   hasLike,
-}: EventPreviewType) => {
+  isRegist,
+}: Props) => {
+  const nav = useNavigate();
   return (
-    <div className="w-[calc((100%-60px)/3)] min-w-[310px] h-[250px] flex flex-col mb-[40px] twocol:w-[calc((100%-60px)/2)] onecol:w-full">
+    <div
+      className={`w-[calc((100%-60px)/3)] min-w-[310px] h-[250px] flex flex-col mb-[40px] twocol:w-[calc((100%-60px)/2)] onecol:w-full${
+        isRegist ? " items-end h-[265px]" : ""
+      }`}
+      onClick={() => nav(`/detail/${programId}`)}
+    >
       <div className="flex gap-[20px]">
-        <div className="w-[140px] h-[185px] flex-shrink-0">
+        <div className="w-[140px] h-[185px] flex-shrink-0 relative">
           <img src={thumbnail} className="w-full h-full object-cover" />
+          {!isOpen && (
+            <div className="w-full h-full top-0 left-0 absolute z-10 bg-black/50 flex justify-center items-center">
+              <div className="font-regular text-smTitle text-white text-center leading-tight">
+                참가 신청 <br /> 종료
+              </div>
+            </div>
+          )}
         </div>
         <div className="w-[calc(100%-160px)] min-w-[150px] flex flex-col">
           <div className="flex justify-between">
@@ -37,10 +55,14 @@ const EventItem = ({
         </div>
       </div>
       {goal && (
-        <div className="flex flex-col mt-[16px]">
+        <div className="w-full flex flex-col mt-[16px]">
           <div className="flex justify-between items-center mb-[10px]">
             <div className="flex items-center gap-[6px]">
-              <div className="font-bold text-mdTitle text-mainBlue">
+              <div
+                className={`font-bold text-mdTitle ${
+                  isOpen ? "text-mainBlue" : "text-darkGray"
+                }`}
+              >
                 {goal.progressRate + "%"}
               </div>
               <div className="font-regular text-sm text-darkGray mt-[5px]">
@@ -51,17 +73,30 @@ const EventItem = ({
               <div className="font-regular text-sm text-darkGray">
                 참가 신청 마감
               </div>
-              <div className="font-regular text-p text-slateBlack">
-                {"D-" + remainingDays}
-              </div>
+              {isOpen && (
+                <div className="font-regular text-p text-slateBlack">
+                  {"D-" + remainingDays}
+                </div>
+              )}
             </div>
           </div>
           <div className="w-full h-[5px] bg-lightGray rounded-full">
             <div
-              className="h-[5px] bg-mainBlue rounded-full"
+              className={`h-[5px] ${
+                isOpen ? "bg-mainBlue" : "bg-darkGray"
+              } rounded-full`}
               style={{ width: `${goal.progressRate}%` }}
             ></div>
           </div>
+        </div>
+      )}
+      {isRegist && (
+        <div
+          className="flex items-center gap-[6px] mt-[16px] cursor-pointer"
+          onClick={() => nav(`/path/${programId}`)}
+        >
+          <div className="font-regular text-p text-black">신청자 조회하기</div>
+          <ArrowCircleIcon />
         </div>
       )}
     </div>
