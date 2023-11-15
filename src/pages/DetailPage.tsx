72b6336386892a0272b1740dbe96f2d50ad56446
서진 <dltjwls0529@gmail.com>
2023-11-15 23:59:31 +0900

    ✨ AuthRoute 구현 및 사용, 라우팅 추가

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import CategoryNavBar from "../components/_common/CategoryNavBar";
 
 const DetailPage = () => {
-  return <></>;
+  const { id } = useParams();
+  return (
+    <>
+      <CategoryNavBar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <a href={`/detail/${id}/register`}>신청하러 가기</a>
+    </>
+  );
 };
 
 export default DetailPage;
