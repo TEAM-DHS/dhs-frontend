f7c6bae51fe44c8495602aa97bf8052b02095931
서진 <dltjwls0529@gmail.com>
2023-11-15 22:57:57 +0900

    ✨ 홈 페이지, 카테고리별 리스트 페이지 구현

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import NavBar from "../components/_common/NavBar";
+import ListSection from "../components/ListPage/ListSection";
+
+import { useRecoilState, useResetRecoilState } from "recoil";
+import { filterState } from "../services/store/event";
 
 const ListPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [filter, setFilter] = useRecoilState(filterState);
+  const resetFilter = useResetRecoilState(filterState);
+  useEffect(() => {
+    resetFilter();
+  }, []);
+  useEffect(() => {
+    setFilter(prev => ({
+      ...prev,
+      category: String(searchParams.get("category")),
+    }));
+  }, [searchParams]);
   return (
     <>
       <NavBar />
+      <ListSection />
     </>
   );
 };
