8528c040a406e28ad2b90c58a2d28b18fc07b386
서진 <dltjwls0529@gmail.com>
2023-11-20 22:44:56 +0900

    ✨ 행사 상세 계획 탭 UI 구현

---
+++ src/components/DetailPage/TabSection.tsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react";
+import PlanTab from "./PlanTab";
+import NoticeTab from "./NoticeTab";
+import LocationTab from "./LocationTab";
+
+export type TabType = "행사 계획" | "공지사항" | "위치";
+const tabList: TabType[] = ["행사 계획", "공지사항", "위치"];
+
+const TabSection = ({ program }: EventDetailType) => {
+  const [tab, setTab] = useState<TabType>("행사 계획");
+  const tabRef = useRef<HTMLDivElement>(null);
+  return (
+    <div className="w-full flex flex-col items-center gap-[60px]">
+      <div
+        ref={tabRef}
+        className="w-full h-[63px] border-t-[1px] border-b-[1px] border-lightGray flex justify-center"
+      >
+        <div className="w-[1160px] laptop:w-[calc(100%-100px)] h-full flex justify-start gap-[10px]">
+          {tabList.map(item => (
+            <div
+              className={`h-full flex items-center font-bold text-smTitle mt-[2px] px-[5px] border-b-[3px] cursor-default ${
+                item === tab
+                  ? " border-black text-black"
+                  : "border-transparent text-darkGray"
+              }`}
+              onClick={() => setTab(item)}
+            >
+              {item}
+              {item == "공지사항" && (
+                <div className="text-sm ml-[5px] mb-[6px]">
+                  {program.notices.length}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      {tab === "행사 계획" ? (
+        <PlanTab {...program} tabRef={tabRef} setTab={setTab} />
+      ) : tab === "공지사항" ? (
+        <NoticeTab />
+      ) : tab === "위치" ? (
+        <LocationTab />
+      ) : null}
+    </div>
+  );
+};
+
+export default TabSection;
