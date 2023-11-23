3356e947a82d4cf284b4554a155bf8b58d9bdfd9
서진 <dltjwls0529@gmail.com>
2023-11-23 15:30:44 +0900

    ✨ program, heart api 함수 작성

---
+++ src/api/heart.ts
@@ -0,0 +1,31 @@
+import client from "./client";
+
+// get heart
+export const getHeart = async () => {
+  try {
+    const res = await client.get("/hearts");
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// post heart
+export const postHeart = async (programId: number) => {
+  try {
+    const res = await client.post("/hearts", { programId: programId });
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// delete heart
+export const deleteHeart = async (programId: number) => {
+  try {
+    const res = await client.delete(`/hearts/${programId}`);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/api/program.ts
@@ -0,0 +1,60 @@
+import client from "./client";
+import useInfiniteScroll from "../utils/hooks/useInfiniteScroll";
+
+// post program
+
+// get program list
+type GetProgramType = {
+  page: number;
+  filter: EventFilterType;
+};
+export const getProgram = async ({ page, filter }: GetProgramType) => {
+  try {
+    const res = await client.get(
+      `/programs?page=${page}&sort=${filter.sort}${
+        filter.keyword === "" ? "" : `&keyword=${filter.keyword}`
+      }${filter.category === "" ? "" : `&category=${filter.category}`}`,
+    );
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+// use program with infinite scroll
+export const useProgramList = (filter: EventFilterType) => {
+  const { pageLastItemRef, data, isFetching, hasNextPage, fetchNextPage } =
+    useInfiniteScroll({
+      queryKey: ["programList", filter],
+      // initialPage: 1,
+      fetch: getProgram,
+      fetchParams: { filter: filter },
+      onIntersect: async (entry, observer) => {
+        observer.unobserve(entry.target);
+        if (hasNextPage && !isFetching) fetchNextPage();
+      },
+    });
+  return { pageLastItemRef, hasNextPage, data };
+};
+
+// get program detail
+export const getProgramDetail = async (programId: number) => {
+  try {
+    const res = await client.get(`/programs/${programId}`);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// post program register
+export const postProgramRegister = async (
+  programId: number,
+  info: EventRegisterFormType,
+) => {
+  try {
+    const res = await client.post(`/programs/${programId}`, info);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
